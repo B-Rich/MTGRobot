5e2482c261b7e8370bb89b21792210e06f5b800a
Justin Woolley <harshinvective@gmail.com>
2017-03-25 23:19:18 -0700

    removing scratch file

--- scripts/scratch/twittermatch.js
+++
@@ -1,5 +0,0 @@
-$('td:contains([twerger]):not(td:has(textarea)), div.search_results:contains([twerger])').each(function() {
-	const twitterIdRegexp = new RegExp('https?://[^/]*twitter.com(?:[^/]*/)*(\\d{4,19})');
-    const twitterId = $(this).html($(this).html().replace(twitterIdRegexp, $1);
-	console.log('found twerger tag:' + twitterId);
-});
